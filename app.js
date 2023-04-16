e4f9395380f2d8e79585eef9b871c714a66581de
mel-stark <ccomelissaoliveira@gmail.com>
2023-04-16 18:42:13 -0300

    Complete desktop

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const https = require("https")
+const bodyParser = require("body-parser")
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static("public"))
+
+app.set('view engine', 'ejs') /* installing ejs */
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html")
-});
-
-app.post("/thank-you", function(req, res) {
-  res.sendFile(__dirname + "/success.html")
 })
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server started on port 3000.");
-});
+app.post('/thank-you', function(req, res) {
+  var chosenRating = req.body.rating;
+  res.render('thank-you.ejs', { chosenRating: chosenRating });
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running")
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,31 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button')
 
 // Get all rating buttons
-const ratingButtons = document.querySelectorAll('.rating-btn');
+const ratingButtons = document.querySelectorAll('.rating-btn')
 
 // Add event listener to each rating button
 ratingButtons.forEach(button => {
   button.addEventListener('click', () => {
     // Set the value of the hidden input field
-    document.getElementById('rating-input').value = button.dataset.rating;
+    document.getElementById('rating-input').value = button.dataset.rating
+
+    // Add the rating value to the submit button link
+    document.getElementById('submit-btn').href = `/thank-you?rating=${button.dataset.rating}`;
 
     // Remove the "active" class from all rating buttons
     ratingButtons.forEach(button => {
-      button.classList.remove('active');
-    });
+      button.classList.remove('active')
+    })
 
     // Add the "active" class to the clicked rating button
-    button.classList.add('active');
+    button.classList.add('active')
 
     // Enable the submit button
-    document.getElementById('submit-btn').disabled = false;
-  });
-});
+    document.getElementById('submit-btn').disabled = false
+
+    // Get the selected rating
+    const selectedRating = document.querySelector('.active').textContent
+
+    console.log(selectedRating)
+  })
+})
