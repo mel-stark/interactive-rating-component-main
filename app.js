75993f734ed9a01a268a9a00c05956f9892a7303
Mel Stark <melyssastark@pm.me>
2023-08-13 13:58:41 -0300

    changes made for deploying

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //jshint esversion:6
 
-const express = require("express")
-const https = require("https")
-const bodyParser = require("body-parser")
+import express from "express";
+import https from "https";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express()
 
@@ -19,7 +22,7 @@ app.get("/", function(req, res){
 
 app.post('/thank-you', function(req, res) {
   var chosenRating = req.body.rating;
-  res.render("public/thank-you.ejs", { chosenRating: chosenRating });
+  res.render("thank-you.ejs", { chosenRating: chosenRating });
 })
 
 app.listen(process.env.PORT || 5415, function () {
